e1d999e6ea91839c1b8c5cb2f79f54b81edbec9b
Fernando Santacruz <94269482+lfSant@users.noreply.github.com>
2023-07-25 18:15:05 -0500

    buscar libro

--- src/main/java/com/ucacue/biblioteca/infraestructure/repository/LibroRepository.java
+++ src/main/java/com/ucacue/biblioteca/infraestructure/repository/LibroRepository.java
@@ -3,6 +3,7 @@ package com.ucacue.biblioteca.infraestructure.repository;
 import com.ucacue.biblioteca.model.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,12 +12,12 @@ import java.util.List;
 public interface LibroRepository extends JpaRepository<Libro, Integer> {
 
     @Query("SELECT l FROM Libro l WHERE l.nombre LIKE %?1%")
-    List<Libro> findByNombre(String nombre);
+    List<Libro> findByNombre(@Param("nombre") String nombre);
 
     @Query("SELECT l FROM Libro l WHERE l.autor LIKE %?1%")
-    List<Libro> findByAutor(String autor);
+    List<Libro> findByAutor(@Param("autor") String autor);
 
     @Query("SELECT l FROM Libro l WHERE l.genero LIKE %?1%")
-    List<Libro> findByGenero(String genero);
+    List<Libro> findByGenero(@Param("genero") String genero);
     Libro save(Libro libro);
 }
