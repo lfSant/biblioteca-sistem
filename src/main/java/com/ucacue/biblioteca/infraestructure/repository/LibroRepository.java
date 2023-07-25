6469c1759cc29e1773006d901488e1d7238e5526
Fernando Santacruz <94269482+lfSant@users.noreply.github.com>
2023-07-25 17:50:18 -0500

    buscar libro

--- src/main/java/com/ucacue/biblioteca/infraestructure/repository/LibroRepository.java
+++ src/main/java/com/ucacue/biblioteca/infraestructure/repository/LibroRepository.java
@@ -2,6 +2,7 @@ package com.ucacue.biblioteca.infraestructure.repository;
 
 import com.ucacue.biblioteca.model.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,9 +10,13 @@ import java.util.List;
 @Repository
 public interface LibroRepository extends JpaRepository<Libro, Integer> {
 
+    @Query("SELECT l FROM Libro l WHERE l.nombre LIKE %?1%")
     List<Libro> findByNombre(String nombre);
+
+    @Query("SELECT l FROM Libro l WHERE l.autor LIKE %?1%")
     List<Libro> findByAutor(String autor);
+
+    @Query("SELECT l FROM Libro l WHERE l.genero LIKE %?1%")
     List<Libro> findByGenero(String genero);
     Libro save(Libro libro);
-
 }
